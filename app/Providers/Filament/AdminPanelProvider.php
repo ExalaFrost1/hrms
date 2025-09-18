47f8c5414bece1e763b33724277b4ce4664f3262
arsal381 <arslanfida@engineer.com>
2025-09-18 21:19:39 +0500

    View Employee Improvements. Navigation Groups.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login(Login::class)
             ->brandLogo('/storage/images/logo_1.png')
-            ->brandLogoHeight('4rem')
+            ->brandLogoHeight('10rem')
             ->topNavigation()
             ->maxContentWidth("full")
             ->colors([
